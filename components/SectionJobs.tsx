2d2a92e5161f944d45fbd3dc5db60af5a2ac4157
Eduardo Kegler <eduardokegler1@gmail.com>
2026-04-07 10:38:03 -0300

    update: add Liquid Labs, fix Coinbase dates and experience years

--- components/SectionJobs.tsx
+++ components/SectionJobs.tsx
@@ -11,22 +11,33 @@ function SectionJobs() {
       <TextLabel1 className="flex-2">EXPERIENCE</TextLabel1>
       <div className="flex flex-10 flex-col gap-10 lg:gap-20">
         <TextTitle2 as="p">
-          Senior Frontend Engineer with 7+ years of experience building
-          highperformance, scalable web applications across Real Estate, Work
-          Safety, and Cryptocurrency sectors. 
+          Senior Frontend Engineer with 8+ years of experience building
+          high-performance, scalable web applications across Real Estate, Work
+          Safety, and Cryptocurrency sectors.
         </TextTitle2>
         <div className="flex flex-col gap-10">
           <div className="flex flex-col gap-4 sm:gap-10">
             <div className="flex flex-col sm:flex-row gap-4">
+              <div className="flex flex-1 flex-col">
+                <TextTitle3>Liquid Labs</TextTitle3>
+                <TextBody color={"level2"}>
+                  Senior Frontend Engineer
+                </TextBody>
+                <TextLabel1 color={"level4"}>
+                  March 2025 — present
+                </TextLabel1>
+              </div>
               <div className="flex flex-1 flex-col">
                 <TextTitle3>Coinbase</TextTitle3>
                 <TextBody color={"level2"}>
                   Senior Software Engineer
                 </TextBody>
                 <TextLabel1 color={"level4"}>
-                  August 2021 - present
+                  August 2021 — March 2025
                 </TextLabel1>
               </div>
+            </div>
+            <div className="flex flex-col sm:flex-row gap-4">
               <div className="flex flex-1 flex-col">
                 <TextTitle3>Tech6</TextTitle3>
                 <TextBody color={"level2"}>Software Engineer</TextBody>
@@ -34,8 +45,6 @@ function SectionJobs() {
                   June 2019 — August 2021
                 </TextLabel1>
               </div>
-            </div>
-            <div className="flex flex-col sm:flex-row gap-4">
               <div className="flex flex-1 flex-col">
                 <TextTitle3>Todobit</TextTitle3>
                 <TextBody color={"level2"}>
@@ -45,6 +54,8 @@ function SectionJobs() {
                   August 2018 — August 2019
                 </TextLabel1>
               </div>
+            </div>
+            <div className="flex flex-col sm:flex-row gap-4">
               <div className="flex flex-1 flex-col">
                 <TextTitle3>Santinvest</TextTitle3>
                 <TextBody color={"level2"}>IT&apos;s Intern</TextBody>
@@ -52,6 +63,7 @@ function SectionJobs() {
                   September 2017 — August 2018
                 </TextLabel1>
               </div>
+              <div className="flex flex-1 flex-col" />
             </div>
           </div>
           <div className="mt-4">
